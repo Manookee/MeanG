b6cbfccd098b94531a5f2ac938d9ca590bbdb21c
ScoobySnacks69 <43790975+ScoobySnacks69@users.noreply.github.com>
2018-10-03 23:57:26 +0300

    UnityProject

---
+++ UnityProjects/MeanGod/Assets/scriptz/OrbitCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// maintains position offset while orbiting around target
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float rotSpeed = 1.5f;
+
+    private float _rotX;
+    private float _rotY;
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rotX = transform.eulerAngles.x;
+        _rotY = transform.eulerAngles.y;
+        _offset = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _rotX -= Input.GetAxis("Vertical") * rotSpeed;
+        _rotY -= Input.GetAxis("Horizontal") * rotSpeed;
+        Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0);
+        transform.position = target.position - (rotation * _offset);
+        transform.LookAt(target);
+    }
+}
---
+++ UnityProjects/MeanGod/Assets/scriptz/RayShooter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour
+{
+    public Camera camera;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform objectHit = hit.transform;
+
+                // Do something with the object that was hit by the raycast.
+                Debug.Log("Hit " + hit.point);
+                StartCoroutine(SphereIndicator(hit.point));
+            }
+        }
+    }
+    private IEnumerator SphereIndicator(Vector3 pos)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+
+        yield return new WaitForSeconds(1);
+
+        Destroy(sphere);
+    }
+}
